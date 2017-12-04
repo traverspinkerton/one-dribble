5a8159afe77179444c662f247de45dfe47267178
Travers Pinkerton <jtpinkerton@gmail.com>
2017-12-04 14:19:13 -0600

    added form submition handler function, calls out out NBA API. Using NBA NPM package for now.

--- src/Components/search_bar.js
+++ src/Components/search_bar.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import NBA from 'nba';
 
 class SearchBar extends Component {
+  state = {
+    inputText: ""
+  }
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    const playerInfo = NBA.findPlayer(this.state.inputText);
+    NBA.stats.playerInfo({ PlayerID: playerInfo.playerId })
+      .then(data => {
+        const playerInfo = [ data.commonPlayerInfo[0], data.playerHeadlineStats[0] ];
+        console.log(playerInfo);
+        this.props.updateList(playerInfo);
+
+      });
+  }
+
   render() {
     return (
       <div className="SearchBar">
-        <form onSubmit={this.handleFormSubmit}>
-          <input placeholder="Enter the name of a player" />
+        <form onSubmit={e => this.handleFormSubmit(e)}>
+          <input placeholder="Enter the name of a player" value={this.state.inputText} onChange={e => this.setState({inputText: e.target.value})}/>
         </form>
       </div>
     )
