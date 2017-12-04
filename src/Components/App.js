2d9959487b69869fb9ae73f5e6c57c29aa2d62c0
Travers Pinkerton <jtpinkerton@gmail.com>
2017-12-04 14:17:47 -0600

    madde list stateful in top level component, passed down to SearchBar and PlayerInfo children

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 import SearchBar from './search_bar';
+import PlayerInfo from './player_info';
 
 class App extends Component {
+  state = {
+    list: []
+  }
+
+  updateList = list => {
+    console.log('app updatelist: ', list);
+    this.setState({list});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">NBA STATS</h1>
+          <h1 className="App-title">One Dribble</h1>
         </header>
-        <SearchBar />
+        <SearchBar list={this.state.list} updateList={this.updateList}/>
+        <PlayerInfo list={this.state.list} />
       </div>
     );
   }
