8d7216f18fd6f72ce00cebcf9673d8bcdd442139
Travers Pinkerton <jtpinkerton@gmail.com>
2017-12-06 11:27:37 -0600

    refactored from nba package to direct get request, displaying basic player information

--- src/Components/player_info.js
+++ src/Components/player_info.js
@@ -2,16 +2,21 @@ import React from 'react';
 
 function PlayerInfo(props) {
   if (props.list[0]) {
+    const about = props.list[0].rowSet[0];
+    const currAvgs = props.list[1].rowSet[0];
+    console.log(currAvgs);
+
     return  (
         <div>
-          <ul>
-            <li>Hi! {props.list[0].displayFirstLast}</li>
-            <li>{props.list[1].playerName}</li>
-          </ul>
+        <h2>{about[3]}</h2>
+        <h3>{about[20]} {about[17]}, {about[14]}, #{about[13]}</h3>
+        <p> {about[10]}, {about[11]} lbs, School/Origin: {about[7]}, Country: {about[8]}</p>
+        <p> {currAvgs[2]} season: {currAvgs[3]} pts, {currAvgs[5]} reb, {currAvgs[4]} ast, PIE {currAvgs[5]}</p>
+
         </div>
       );
   } else {
-    return <div>heya there</div>;
+    return <div></div>;
   }
 }
 
